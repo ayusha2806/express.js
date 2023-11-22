7ff557c93ea6b91508049be851bfd2f8880a087e
ayusha2806 <anandayush2806@gmail.com>
2023-11-22 13:43:46 +0530

    body-parser

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
-app.use(function(req,res,next){
-    console.log("In The Middleware!");
-    next();
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method ="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
 })
 
-app.use(function(req,res,next){
-    console.log("In The Middleware!");
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
     res.send("<h1>Hello From Express.Js!</h1>")
 })
 
