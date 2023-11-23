99cc1dd3534b7c5cb0b4dacef9cab6721b502e0f
ayusha2806 <anandayush2806@gmail.com>
2023-11-23 10:33:36 +0530

    routers and filter

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
 
-app.use(bodyParser.urlencoded({extended:false}));
+const express = require('express');
+const bodyParser=require('body-parser');
+const app = express();
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method ="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
 })
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/',(req,res,next)=>{
-    res.send("<h1>Hello From Express.Js!</h1>")
-})
-
-app.listen(3000)
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
