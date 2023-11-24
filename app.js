05db37f414f7a63b8d21b917f1bcd8886094b47e
ayusha2806 <anandayush2806@gmail.com>
2023-11-24 15:03:27 +0530

    Serving HTML , CSS and JS (VIEWS)

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-
+const path = require('path');
 const express = require('express');
-const bodyParser=require('body-parser');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-const adminRoutes=require('./routes/admin');
-const shopRoutes=require('./routes/shop');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
-})
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));  
 });
+
+app.listen(3000);
