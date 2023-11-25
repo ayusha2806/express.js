0db06a8be8fc2a69a186fa74160e2b1f1bbf48db
ayusha2806 <anandayush2806@gmail.com>
2023-11-25 17:03:18 +0530

    models

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,24 @@
-const path=require('path');
+const path = require('path');
 
-const express=require('express');
+const express = require('express');
 
-const rootDir=require('../util/path');
+const adminController = require('../controllers/admin');
 
-const router=express.Router();
+const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views/add-product.html'));
-});
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/add-product',(req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-module.exports=router;
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
+
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
+
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,25 @@
-const path=require('path');
+const path = require('path');
 
-const express=require('express');
-const rootDir=require('../util/path');
+const express = require('express');
 
-const router=express.Router();
+const shopController = require('../controllers/shop');
 
-router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(rootDir,'views/shop.html'));
-});
+const router = express.Router();
 
-module.exports=router; 
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
+
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
