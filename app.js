c732770b7a8fb208c36d3eed51abe79dc610ab97
ayusha2806 <anandayush2806@gmail.com>
2023-11-28 22:34:40 +0530

    Retrieving data from databases

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const path = require('path');
 
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database')
+const db=require('./util/database')
 
 const app = express();
 
@@ -15,7 +14,11 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * FROM products')
+db.execute('SELECT * FROM products').then(result=>{
+    console.log(result)
+}).catch(err=>{
+    console.log(err);
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
--- util/database.js
+++ util/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'node-complete',
-    password: 'nodecomplete'
+    password: 'ayush2anand',
+    database: 'node-complete'
 });
 
 module.exports = pool.promise();
